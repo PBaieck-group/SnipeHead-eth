d6cdc7fa46b30db1b6c4bcc2582963401f646c27
PBaieck <efaitcome777@proton.me>
2026-09-10 22:37:17 -0400

    feat: add IPFS fallback links to Addresses.sol

--- src/Addresses.sol
+++ src/Addresses.sol
@@ -6,4 +6,6 @@ library Addresses {
     address public constant SHD_TOKEN = 0xa1e1a6cB1F694e41a5C270542dF233673665FCd5;
     address public constant MINING_CONTRACT = 0x0AF20FaC296a4f19C68acC64FB850b699D6869e3;
     address public constant SNFT_CONTRACT = 0xE85eB0a14E185968B2a947d98feA023F8e80AB3a;
+    address public constant SNFT_IPFS_dAPP = https://bafybeia6n6uyxr5mbuwseezxczugvjdotjipb7y4dnrxif6cirxsnrizie.ipfs.inbrowser.link/
+    address public constant SNFT_IPFS_dAPP = https://bafybeibmc5fipcyunvlofyf47ileagkl7hgbnwrapbbjjgkspiwgiqswtm.ipfs.inbrowser.link/
 }

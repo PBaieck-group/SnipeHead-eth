5ca69bef4165ff46a09dc88b42d88151757cfbac
PBaieck <efaitcome777@proton.me>
2026-09-10 22:59:29 -0400

    feat: fix IPFS fallback link in Addresses.sol

--- src/Addresses.sol
+++ src/Addresses.sol
@@ -7,5 +7,5 @@ library Addresses {
     address public constant MINING_CONTRACT = 0x0AF20FaC296a4f19C68acC64FB850b699D6869e3;
     address public constant SNFT_CONTRACT = 0xE85eB0a14E185968B2a947d98feA023F8e80AB3a;
     //string public constant SNFT_IPFS_dAPP ="https://bafybeia6n6uyxr5mbuwseezxczugvjdotjipb7y4dnrxif6cirxsnrizie.ipfs.inbrowser.link/";
-    //string public constant SNFT_IPFS_dAPP ="https://bafybeibmc5fipcyunvlofyf47ileagkl7hgbnwrapbbjjgkspiwgiqswtm.ipfs.inbrowser.link/";
+    //string public constant MINING_IPFS_dAPP ="https://bafybeibmc5fipcyunvlofyf47ileagkl7hgbnwrapbbjjgkspiwgiqswtm.ipfs.inbrowser.link/";
 }
